f0c21eaee8e4d2fb694c5eea74de9f097cf7375e
Arek <aguminski@gmail.com>
2019-05-05 22:01:43 +0200

    Custom user model registered in admin

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3gfw$50@mqo8q91m^+n+styoud69s&3=5&sj4*!44grc^&)mw('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
